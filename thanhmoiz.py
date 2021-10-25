e7166cbf78dd5bebbdd807f2ae7d250379c8edb4
SaintLaurentxTran <thanhmoicotoi@gmail.com>
2021-10-25 18:46:05 +0700

    add thanhmoiz.py

---
+++ thanhmoiz.py
@@ -0,0 +1,7 @@
+yearOfBirth = int(input('Type your year of birth: '))
+age = 2021 - yearOfBirth
+print(f'You are {age} years old')
+if age < 18:
+    print("Chưa đủ tuổi đi tù")
+else:
+    print("Đã đủ tuổi đi tù")
